f01f5cf545066d9132640e0da6707d3cb5af186f
dylann97 <d.xavero@hotmail.com>
2023-11-26 13:26:28 +0100

    🎨 F02: App > Setting Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
 import { LoginPage } from "./components/pages/login/LoginPage";
+import OrderPage from "./components/pages/orderpage/OrderPage";
+import ErrorPage from "./components/pages/ErrorPage/ErrorPage";
 
 function App() {
-  return <LoginPage />;
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <LoginPage />,
+      errorElement: <ErrorPage />,
+    },
+    {
+      path: "/order",
+      element: <OrderPage />,
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
