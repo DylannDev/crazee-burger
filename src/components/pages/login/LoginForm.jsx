98522785f3173d1ce12346dcdb4776c1f8bd42c8
dylann97 <d.xavero@hotmail.com>
2023-11-27 18:52:00 +0100

    💄 F03: LoginPage > add layout

--- src/components/pages/login/LoginForm.jsx
+++ src/components/pages/login/LoginForm.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
 
 export default function LoginForm() {
   // states
@@ -18,7 +19,7 @@ export default function LoginForm() {
   };
 
   return (
-    <form action="submit" onSubmit={handleSubmit}>
+    <LoginFormStyled action="submit" onSubmit={handleSubmit}>
       <h1>Bienvenue chez nous !</h1>
       <br />
       <h2>Connectez-vous</h2>
@@ -26,10 +27,17 @@ export default function LoginForm() {
         onChange={handleChange}
         value={input}
         type="text"
-        placeholder="Entrez votre prénom..."
+        placeholder="Entrez votre prénom"
         required
       />
-      <button>Accédez à votre espace</button>
-    </form>
+      <button>Accéder à mon espace</button>
+    </LoginFormStyled>
   );
 }
+
+const LoginFormStyled = styled.form`
+  background: green;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
--- src/components/pages/login/LoginPage.jsx
+++ src/components/pages/login/LoginPage.jsx
@@ -1,9 +1,21 @@
+import styled from "styled-components";
 import LoginForm from "./LoginForm";
+import Logo from "../../reusable-ui/Logo";
 
 export const LoginPage = () => {
   return (
-    <div>
+    <LoginPageStyled>
+      <Logo />
       <LoginForm />
-    </div>
+    </LoginPageStyled>
   );
 };
+
+const LoginPageStyled = styled.div`
+  background: red;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
