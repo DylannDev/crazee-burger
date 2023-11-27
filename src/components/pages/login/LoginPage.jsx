751b3e37f1b2bf7b6a6ec24f3e332c48c88b3f7f
dylann97 <d.xavero@hotmail.com>
2023-11-27 20:16:38 +0100

    💄 F03: Logo > Add styling

--- src/components/pages/login/LoginPage.jsx
+++ src/components/pages/login/LoginPage.jsx
@@ -17,19 +17,14 @@ const LoginPageStyled = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-
-  ::before {
-    content: "";
-    background: url("/images/burger-background.jpg") rgba(0, 0, 0, 0.6);
-    background-size: cover;
-    background-position: center;
-    background-blend-mode: darken;
-
-    position: absolute;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    z-index: -1;
-  }
+  background: url("/images/burger-background.jpg") rgba(0, 0, 0, 0.6);
+  background-size: cover;
+  background-position: center;
+  background-blend-mode: darken;
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  z-index: -1;
 `;
--- src/components/reusable-ui/Logo.jsx
+++ src/components/reusable-ui/Logo.jsx
@@ -1,9 +1,37 @@
 import styled from "styled-components";
 
 export default function Logo() {
-  return <LogoStyled>Logo</LogoStyled>;
+  return (
+    <LogoStyled>
+      <h1>crazee</h1>
+      <img src="/images/logo-crazee-burger.png" alt="logo crazee burger" />
+      <h1>burger</h1>
+    </LogoStyled>
+  );
 }
 
 const LogoStyled = styled.div`
-  background: blue;
+  display: flex;
+  align-items: center;
+  transform: scale(2.5);
+
+  h1 {
+    display: inline;
+    text-align: center;
+    color: #ffa01b;
+    font-size: 36px;
+    line-height: 1em;
+    font-weight: 700;
+    text-transform: uppercase;
+    letter-spacing: 1.5px;
+    font-family: "Amatic SC", cursive;
+  }
+
+  img {
+    object-fit: contain;
+    object-position: center;
+    height: 60px;
+    width: 80px;
+    margin: 0 5px;
+  }
 `;
