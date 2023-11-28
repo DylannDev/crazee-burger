ede57480864a93a3e3124bb058dd824b85503f8f
dylann97 <d.xavero@hotmail.com>
2023-11-28 22:35:26 +0100

    ♻️ F04: Navbar > Create component Profile

--- src/components/pages/OrderPage/NavbarRightSide.jsx
+++ src/components/pages/OrderPage/NavbarRightSide.jsx
@@ -1,21 +1,12 @@
 /* eslint-disable react/prop-types */
-import { Link } from "react-router-dom";
-import { BsPersonCircle } from "react-icons/bs";
 import styled from "styled-components";
 import { theme } from "../../../theme";
+import Profile from "./Profile";
 
 export default function NavbarRightSide({ username }) {
   return (
     <NavbarRightSideStyled>
-      <div className="user-infos">
-        <h1>
-          Hey, <span>{username}</span>
-        </h1>
-        <Link to="/">
-          <span>Se déconnecter</span>
-        </Link>
-      </div>
-      <BsPersonCircle className="icon" />
+      <Profile username={username} />
     </NavbarRightSideStyled>
   );
 }
@@ -25,44 +16,4 @@ const NavbarRightSideStyled = styled.div`
   align-items: center;
   gap: 10px;
   color: ${theme.colors.greyBlue};
-
-  .user-infos {
-    display: flex;
-    flex-direction: column;
-    align-items: flex-end;
-    gap: ${theme.gridUnit / 2}px;
-    font-family: Open Sans;
-
-    h1 {
-      font-size: 16px;
-      font-weight: 400;
-      margin: 0;
-
-      span {
-        color: ${theme.colors.primary};
-        font-weight: 700;
-      }
-    }
-
-    a {
-      font-size: 10px;
-      font-weight: 400;
-      text-decoration: none;
-      background-color: transparent;
-      color: ${theme.colors.greyBlue};
-      padding: 0 0 ${theme.spacing.xxs} 0;
-      text-align: right;
-      border: none;
-      cursor: pointer;
-    }
-
-    a:hover {
-      text-decoration: underline;
-      text-underline-offset: ${theme.spacing.xxs};
-    }
-  }
-
-  .icon {
-    font-size: ${theme.fonts.size.P4};
-  }
 `;
