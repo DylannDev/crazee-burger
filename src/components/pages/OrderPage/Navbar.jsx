4660f26cef7f8f6914853adb51fbbb00a22665f4
dylann97 <d.xavero@hotmail.com>
2023-11-28 18:50:52 +0100

    ♻️ F04: Navbar > Create component NavbarRightSide

--- src/components/pages/OrderPage/Navbar.jsx
+++ src/components/pages/OrderPage/Navbar.jsx
@@ -1,9 +1,9 @@
 /* eslint-disable react/prop-types */
-import { Link } from "react-router-dom";
-import { BsPersonCircle } from "react-icons/bs";
+
 import Logo from "../../reusable-ui/Logo";
 import styled from "styled-components";
 import { theme } from "../../../theme";
+import NavbarRightSide from "./NavbarRightSide";
 
 export default function Navbar({ username }) {
   return (
@@ -11,17 +11,7 @@ export default function Navbar({ username }) {
       <div className="left-side">
         <Logo />
       </div>
-      <div className="right-side">
-        <div className="user-infos">
-          <h1>
-            Hey, <span>{username}</span>
-          </h1>
-          <Link to="/">
-            <span>Se déconnecter</span>
-          </Link>
-        </div>
-        <BsPersonCircle className="icon" />
-      </div>
+      <NavbarRightSide username={username} />
     </NavbarStyled>
   );
 }
@@ -37,52 +27,5 @@ const NavbarStyled = styled.nav`
 
   .left-side {
     cursor: pointer;
-  }
-
-  .right-side {
-    display: flex;
-    align-items: center;
-    gap: 10px;
-    color: ${theme.colors.greyBlue};
-
-    .user-infos {
-      display: flex;
-      flex-direction: column;
-      align-items: flex-end;
-      gap: ${theme.gridUnit / 2}px;
-      font-family: Open Sans;
-
-      h1 {
-        font-size: 16px;
-        font-weight: 400;
-        margin: 0;
-
-        span {
-          color: ${theme.colors.primary};
-          font-weight: 700;
-        }
-      }
-
-      a {
-        font-size: 10px;
-        font-weight: 400;
-        text-decoration: none;
-        background-color: transparent;
-        color: ${theme.colors.greyBlue};
-        padding: 0 0 ${theme.spacing.xxs} 0;
-        text-align: right;
-        border: none;
-        cursor: pointer;
-      }
-
-      a:hover {
-        text-decoration: underline;
-        text-underline-offset: ${theme.spacing.xxs};
-      }
-    }
-
-    .icon {
-      font-size: ${theme.fonts.size.P4};
-    }
   }
 `;
