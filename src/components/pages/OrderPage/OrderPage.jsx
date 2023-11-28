6e109e2481794f158c38123e99db56cbe6dd8bed
dylann97 <d.xavero@hotmail.com>
2023-11-28 17:42:37 +0100

    💄 F04: OrderPage > Add styling to the layout

--- src/components/pages/OrderPage/OrderPage.jsx
+++ src/components/pages/OrderPage/OrderPage.jsx
@@ -1,5 +1,8 @@
 import { Link, useParams } from "react-router-dom";
 import styled from "styled-components";
+import { BsPersonCircle } from "react-icons/bs";
+import Logo from "../../reusable-ui/Logo";
+import { theme } from "../../../theme";
 
 export default function OrderPage() {
   const { username } = useParams();
@@ -7,10 +10,23 @@ export default function OrderPage() {
   return (
     <OrderPageStyled>
       <div className="container">
-        <h1>Bonjour {username}</h1>
-        <Link to="/">
-          <button>Déconnexion</button>
-        </Link>
+        <nav>
+          <div className="logo">
+            <Logo />
+          </div>
+          <div className="user-infos-icon">
+            <div className="user-infos">
+              <h1>
+                Hey, <span>{username}</span>
+              </h1>
+              <Link to="/">
+                <span>Se déconnecter</span>
+              </Link>
+            </div>
+            <BsPersonCircle className="icon" />
+          </div>
+        </nav>
+        <main></main>
       </div>
     </OrderPageStyled>
   );
@@ -29,5 +45,76 @@ const OrderPageStyled = styled.div`
     background: red;
     height: 95vh;
     width: 1400px;
+    display: flex;
+    flex-direction: column;
+
+    nav {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      border-radius: ${theme.borderRadius.extraRound}
+        ${theme.borderRadius.extraRound} 0px 0px;
+      background: ${theme.colors.background_white};
+      padding: 0px 70px 0px 20px;
+
+      .logo {
+        cursor: pointer;
+      }
+
+      .user-infos-icon {
+        display: flex;
+        align-items: center;
+        gap: 10px;
+        color: ${theme.colors.greyBlue};
+
+        .user-infos {
+          display: flex;
+          flex-direction: column;
+          align-items: flex-end;
+          gap: ${theme.gridUnit / 2}px;
+          font-family: Open Sans;
+
+          h1 {
+            font-size: 16px;
+            font-weight: 400;
+            margin: 0;
+
+            span {
+              color: ${theme.colors.primary};
+              font-weight: 700;
+            }
+          }
+
+          a {
+            font-size: 10px;
+            font-weight: 400;
+            text-decoration: none;
+            background-color: transparent;
+            color: ${theme.colors.greyBlue};
+            padding: 0 0 ${theme.spacing.xxs} 0;
+            text-align: right;
+            border: none;
+            cursor: pointer;
+          }
+
+          a:hover {
+            text-decoration: underline;
+            text-underline-offset: ${theme.spacing.xxs};
+          }
+        }
+
+        .icon {
+          font-size: ${theme.fonts.size.P4};
+        }
+      }
+    }
+
+    main {
+      height: 100%;
+      background: ${theme.colors.background_white};
+      border-radius: 0px 0px ${theme.borderRadius.extraRound}
+        ${theme.borderRadius.extraRound};
+      box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
+    }
   }
 `;
