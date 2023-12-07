e35de8d921a875cc0abff55087a6ffaab65e83c7
dylann97 <d.xavero@hotmail.com>
2023-12-07 12:47:41 +0100

    🚧 F08: AddForm > Add inputs layout

--- src/components/pages/order/Main/Admin/AdminPanel/AddForm.jsx
+++ src/components/pages/order/Main/Admin/AdminPanel/AddForm.jsx
@@ -1,3 +1,43 @@
+import styled from "styled-components";
+
 export default function AddForm() {
-  return <div>AddForm</div>;
+  return (
+    <AddFormStyled>
+      <div className="image-preview">Image preview</div>
+      <div className="input-fields">
+        <input type="text" placeholder="Nom" />
+        <input type="text" placeholder="Image URL" />
+        <input type="text" placeholder="Prix" />
+      </div>
+      <button className="submit-btn">Submit button</button>
+    </AddFormStyled>
+  );
 }
+const AddFormStyled = styled.form`
+  border: 2px solid black;
+  width: 70%;
+  height: 100%;
+
+  display: grid;
+  grid-template-columns: 1fr 3fr;
+  grid-template-rows: repeat(4, 1fr);
+
+  .image-preview {
+    background: red;
+    grid-area: 1 / 1 / 4 / 2;
+  }
+  .input-fields {
+    background: blue;
+    grid-area: 1 / 2 / 4 / 3;
+
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-template-rows: repeat(3, 1fr);
+  }
+  .submit-btn {
+    background: pink;
+    grid-area: 4 / -2 / -1 / -1;
+
+    width: 50%;
+  }
+`;
--- src/components/pages/order/Main/Admin/AdminPanel/AdminPanel.jsx
+++ src/components/pages/order/Main/Admin/AdminPanel/AdminPanel.jsx
@@ -13,8 +13,8 @@ export default function AdminPanel({ currentTabSelected }) {
 
 const AdminPanelStyled = styled.div`
   height: 250px;
+  padding: 30px 5%;
   background: ${theme.colors.white};
-  /* border-top: 1px solid ${theme.colors.quaternary}; */
   box-shadow: 0px -2px 5px 0px rgba(0, 0, 0, 0.1);
   -webkit-box-shadow: 0px -2px 5px 0px rgba(0, 0, 0, 0.1);
   -moz-box-shadow: 0px -2px 5px 0px rgba(0, 0, 0, 0.1);
