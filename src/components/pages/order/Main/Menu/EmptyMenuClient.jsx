0829053a23363385b4f9de08cf9312c9d33f2e81
dylann97 <d.xavero@hotmail.com>
2023-12-09 14:54:12 +0100

    💄 F08: Menu > Add EmptyMenu styles (in Admin and Client mode)

--- src/components/pages/order/Main/Menu/EmptyMenuClient.jsx
+++ src/components/pages/order/Main/Menu/EmptyMenuClient.jsx
@@ -1,3 +1,37 @@
+import styled from "styled-components";
+import { theme } from "../../../../../theme";
+
 export default function EmptyMenuClient() {
-  return <span>Pas de produit pour le client</span>;
+  return (
+    <EmptyMenuClientStyled>
+      <div className="container">
+        <h2>Victime de notre succès ! 😊</h2>
+        <p>De nouvelles recettes sont en cours de préparation.</p>
+        <p>À très vite ! 🍕</p>
+      </div>
+    </EmptyMenuClientStyled>
+  );
 }
+
+const EmptyMenuClientStyled = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 10%;
+
+  .container {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
+    gap: 15px;
+    padding: 30px;
+    color: ${theme.colors.secondary};
+    background: ${theme.colors.white};
+    border-radius: ${theme.borderRadius.round};
+
+    h2 {
+      margin: 0;
+    }
+  }
+`;
