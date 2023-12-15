307a31d4122d4604714f024d569f49b71691bd91
dylann97 <d.xavero@hotmail.com>
2023-12-15 12:01:29 +0100

    🐛 F09: array > Fix bug of function createCopy

--- src/context/AdminContext.jsx
+++ src/context/AdminContext.jsx
@@ -2,14 +2,15 @@
 import { createContext, useState } from "react";
 import { MenuData } from "../MenuData/MenuData";
 import { EMPTY_PRODUCT } from "../enums/product";
+import { createCopy } from "../utils/array";
 
 export const AdminContext = createContext({});
 
 export const AdminContextProvider = ({ children }) => {
   // States
-  const [isModeAdmin, setIsModeAdmin] = useState(true);
-  const [isCollapsed, setIsCollapsed] = useState(true);
-  const [currentTabSelected, setCurrentTabSelected] = useState("edit");
+  const [isModeAdmin, setIsModeAdmin] = useState(false);
+  const [isCollapsed, setIsCollapsed] = useState(false);
+  const [currentTabSelected, setCurrentTabSelected] = useState("add");
   const [isSubmitted, setIsSubmitted] = useState(false);
   const [menu, setMenu] = useState(MenuData);
   const [selectedProduct, setSelectedProduct] = useState(EMPTY_PRODUCT);
@@ -17,7 +18,7 @@ export const AdminContextProvider = ({ children }) => {
   // Comportements
   const handleAddProduct = (newProduct) => {
     // 1 - copie du state
-    const menuCopy = JSON.parse(JSON.stringify(menu));
+    const menuCopy = createCopy(menu);
 
     // 2 - Manipulation de la copie du state
     const newMenu = [newProduct, ...menuCopy];
@@ -28,7 +29,7 @@ export const AdminContextProvider = ({ children }) => {
   };
 
   const handleDeleteProduct = (ProductIdToDelete) => {
-    const menuCopy = JSON.parse(JSON.stringify(menu));
+    const menuCopy = createCopy(menu);
 
     const newMenu = menuCopy.filter(
       (product) => product.id !== ProductIdToDelete
@@ -39,7 +40,7 @@ export const AdminContextProvider = ({ children }) => {
 
   const handleEditProduct = (productToEdit) => {
     // 1 - copie du state (deep clone)
-    const menuCopy = JSON.parse(JSON.stringify(menu));
+    const menuCopy = createCopy(menu);
 
     // 2 - Manipulation de la copie du state
     const indexOfProductToEdit = menuCopy.findIndex(
--- src/utils/array.js
+++ src/utils/array.js
@@ -1,3 +1,3 @@
 export const createCopy = (array) => {
-  JSON.parse(JSON.stringify(array));
+  return JSON.parse(JSON.stringify(array));
 };
