44f2672df18f692aa311157bbee26ba9275b8179
dylann97 <d.xavero@hotmail.com>
2023-12-22 14:05:48 +0100

    ✨ F11: Cart > Create function to add product in basket and increment the quantity of product already in basket

--- src/hooks/useHandleCart.js
+++ src/hooks/useHandleCart.js
@@ -1,12 +1,37 @@
 import { useState } from "react";
 import { MenuData } from "../MenuData/MenuData";
+import { createCopy, findInArray } from "../utils/array";
 
 export const useHandleCart = () => {
   const [cart, setCart] = useState(MenuData.EMPTY);
 
-  const handleAddToBasket = (productToAdd) => {
-    //
+  const handleAddToCart = (productToAdd) => {
+    // 1 - copie du state
+    const cartCopy = createCopy(cart);
+
+    const isProductAlreadyInCart = findInArray(cartCopy, productToAdd.id);
+
+    if (!isProductAlreadyInCart) {
+      const newCartProduct = {
+        ...productToAdd,
+        quantity: 1,
+      };
+
+      // 2 - Manipulation de la copie du state
+      const cartUpdated = [newCartProduct, ...cartCopy];
+
+      // 3 - Update du state
+      setCart(cartUpdated);
+    } else {
+      const indexOfProductInCartToIncrement = cartCopy.findIndex(
+        (cartProduct) => cartProduct.id === productToAdd.id
+      );
+
+      cartCopy[indexOfProductInCartToIncrement].quantity += 1;
+
+      setCart(cartCopy);
+    }
   };
 
-  return { cart, handleAddToBasket };
+  return { cart, handleAddToCart };
 };
